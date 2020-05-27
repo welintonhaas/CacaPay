fe21cb878f90e153c1eccb8ccef7c8914b991088
Welinton Haas <welinton.haas@gmail.com>
2020-05-26 22:54:31 -0300

    Migrations

---
+++ database/migrations/2020_05_26_225254_create_clientes.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('cpf');
+            $table->string('telefone');
+            $table->string('email');
+            $table->string('senha');
+            $table->bigInteger('conta');
+            $table->bigInteger('idCidade')->unsigned();
+            $table->double('saldo', 15, 2)->nullable()->default(0.00);
+            $table->foreign('idCidade')->references('id')->on('cidades');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
---
+++ database/migrations/2020_05_27_015001_create_creditos.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCreditos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('creditos', function (Blueprint $table) {
+            $table->id();
+            $table->double('valor',15,2);
+            $table->bigInteger('idCliente')->unsigned();
+            $table->foreign('idCliente')->references('id')->on('clientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('creditos');
+    }
+}
