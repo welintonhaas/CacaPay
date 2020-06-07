a14de7d3d97822e5b0f5cb270316c9ea367274a4
Rui Batista <ruibatistas@gmail.com>
2020-06-07 14:46:46 -0300

    cadastro de clientes

---
+++ app/CadastraCliente.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CadastraCliente extends Model{
+    protected $table = 'cadastra_cliente';
+    protected $primarykey = 'id';
+}
--- resources/views/cad_cliente.blade.php
+++ resources/views/cad_cliente.blade.php
@@ -4,7 +4,43 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-           Fazer -> Formulario de cadastro de Cliente
+            <div class="container-fluid" col-md-6 offset-md-3>
+
+        <h1 class="display-2 text-center">Cadastro de Clientes</h1>
+        <form method="post" action="{{ route('cad_cliente') }}">
+            @csrf
+
+            <div class="form-row">
+                <div class="col-md-2 mb-3">
+                    <label> Digite seu nome</label>
+                        <input type="text" name="nome" placeholder="nome">
+                </div>
+                <div class="col-md-2 mb-3">
+                    <label> Digite seu CPF</label>
+                        <input type="text" name="cpf" placeholder="cpf">
+                </div>
+                <div class="col-md-2 mb-3">
+                    <label>Digite seu Telefone</label>
+                        <input type="text" name="telefone" placeholder="Telefone">                    
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col-md-2 mb-3">
+                    <label>Digite sua cidade</label>
+                        <input type="text" name="cidade" placeholder="cidade">                    
+                </div>
+            <div class="col-md-2 mb-3">
+                    <label>Digite seu Estado</label>
+                        <input type="text" name="state" placeholder="estado">                 
+            </div> 
+            
+
+            </div>
+            <div class="col-md-2 mb-3">
+                         <input type="submit" class="btn btn-primary value = Enviar Cadastro">               
+            </div> 
+
+            </div>  
         </div>
     </div>
 </div>
