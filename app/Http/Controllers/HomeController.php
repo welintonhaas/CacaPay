72f8e01265836c5e7fd1051e44f58b45e63d7cb9
Welinton Haas <welinton.haas@gmail.com>
2020-06-10 20:46:34 -0300

    correção conta Cliente

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Transacoes;
 use App\User;
+use App\Cliente;
 
 class HomeController extends Controller
 {
@@ -31,9 +32,19 @@ class HomeController extends Controller
 
     public function pagamentos()
     {   
+        // Obtem o id do usuário da sessão
         $idUser = Auth::id();
+
+        // A partir do usuário da sessão, obtem o id de cliente associado
         $idCliente = User::find($idUser)->idCliente;
-        $tr = Transacoes::where('idCliente',$idCliente)->get();
-        return view('pagamentos', ['transacoes'=>$tr]);
+
+        // Obtem os dados do cliente 
+        $cliente = Cliente::find($idCliente);
+
+        //Obtem as transações do cliente
+        $transacoes = Transacoes::where('idCliente',$idCliente)->get();
+
+        // Retorna para a view os dados 
+        return view('pagamentos', ['transacoes'=>$transacoes, 'cliente'=>$cliente]);
     }
 }
--- resources/views/pagamentos.blade.php
+++ resources/views/pagamentos.blade.php
@@ -3,31 +3,30 @@
 @section('content')
 
 <div class="container">
-  <h1 class="display-4 mt-2 text-center">Seus Pagamentos Realizados</h1>
+  <h1 class="display-4 mt-2 mb-5 text-center">Seus Pagamentos Realizados</h1>
 
+  <h2>Conta # {{ str_pad($cliente->conta, 10, "0", STR_PAD_LEFT) }}</h2>
   @if (!$transacoes->isEmpty())
   
     <table class="table mt-4">
         <thead>
           <tr>
-            <th scope="col">Conta</th>
-            <th scope="col">Valor</th>
             <th scope="col">Empresa</th>
             <th scope="col">Data e Hora</th>
             <th scope="col">Status</th>
+            <th scope="col">Valor</th>
           </tr>
         </thead>
-        <tbody>          
+        <tbody>                
           @foreach($transacoes as $t)
             <tr>
-              <th scope="row">{{ str_pad($t->conta, 10, "0", STR_PAD_LEFT) }}</th>
-              <td>R$ {{ number_format($t->valor,2,',','') }}</td>
               <td>{{ $t->empresa->razaoSocial }}</td>
               <td>{{ date('d/m/Y H:i:s', strtotime($t->data)) }}</td>
               <td>{{ $t->status->nome }}</td>
+              <td>R$ {{ number_format($t->valor,2,',','') }}</td>
             </tr>
           @endforeach
-          //TODO Adicionar Saldo
+          {{-- TODO Adicionar Saldo --}}
         </tbody>
       </table>
   </div>
